cea6e5a8d0a09dc2347de0e8d0ff3c5af6396d5e
Seiji Sogabe <s.sogabe@gmail.com>
2011-12-05 23:39:37 +0900

    chnaged artifactId and etc etc

--- src/main/java/fr/willdurand/jenkins/karotz/KarotzClient.java
+++ src/main/java/fr/willdurand/jenkins/karotz/KarotzClient.java
@@ -1,26 +1,18 @@
 package fr.willdurand.jenkins.karotz;
 
 import hudson.ProxyConfiguration;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Random;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.logging.Logger;
-
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
@@ -29,108 +21,110 @@ import org.xml.sax.InputSource;
 
 /**
  * KarotzClient class.
- * 
+ *
  * @author William Durand <william.durand1@gmail.com>
  */
 public class KarotzClient {
-	/**
-	 * API Key
-	 */
-	private final static String KAROTZ_API_KEY = "8021fe73-8a57-48a6-828c-3302927b7389";
-	/**
-	 * API Secret
-	 */
-	private final static String KAROTZ_API_SECRET = "";
+
+    /**
+     * API Key
+     */
+    private final static String KAROTZ_API_KEY = "8021fe73-8a57-48a6-828c-3302927b7389";
+
+    /**
+     * API Secret
+     */
+    private final static String KAROTZ_API_SECRET = "";
+
     /**
      * Base URL for the START method (auth)
      */
     private final static String KAROTZ_URL_START = "http://api.karotz.com/api/karotz/start";
-	/**
-	 * Base URL for the TTS function
-	 */
-	private final static String KAROTZ_URL_TTS = "http://api.karotz.com/api/karotz/tts";
-	/**
-	 * Base URL for the LED function
-	 */
-	private final static String KAROTZ_URL_LED = "http://api.karotz.com/api/karotz/led";
+
+    /**
+     * Base URL for the TTS function
+     */
+    private final static String KAROTZ_URL_TTS = "http://api.karotz.com/api/karotz/tts";
+
+    /**
+     * Base URL for the LED function
+     */
+    private final static String KAROTZ_URL_LED = "http://api.karotz.com/api/karotz/led";
+
     /**
      * Logger
      */
     private static Logger logger = Logger.getLogger(KarotzClient.class.getName());
-	/**
-	 * Interactive Id
-	 */
-	private static String interactiveId = null;
-	/**
-	 * Install Id
-	 */
-	private String installId = null;
-	
-	/**
-	 * Default constructor
-	 */
-	public KarotzClient(String installId) {
-		this.installId 	= installId;
-	}
-	
+
+    /**
+     * Interactive Id
+     */
+    private static String interactiveId = null;
+
+    /**
+     * Install Id
+     */
+    private String installId = null;
+
+    /**
+     * Default constructor
+     */
+    public KarotzClient(String installId) {
+        this.installId = installId;
+    }
+
     /**
      * @return String
      */
-	public String getInstallId() {
-		return installId;
-	}
+    public String getInstallId() {
+        return installId;
+    }
 
-	/**
-	 * @return String
-	 */
-	public String getInteractiveId()
-	{
-		if (null == KarotzClient.interactiveId) {
-			this.requestInteractiveId();
-		}
-		return KarotzClient.interactiveId;
-	}
-	
-	/**
-	 * @param textToSpeak
-	 * @param language
-	 * @return
-	 */
-	public boolean speak(String textToSpeak, String language)
-	{
-		textToSpeak = textToSpeak.replace(" ", "+");
-		/*
-		try {
-			textToSpeak = URLEncoder.encode(textToSpeak, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			logger.severe("Error encoding TTS: " + e.getMessage());
-		}
-		*/
-		
-		String url = KAROTZ_URL_TTS + "?action=speak&lang=" + language + "&text="
-			+ textToSpeak + "&interactiveid=" + this.getInteractiveId();
-        
-		return this.doRequest(url);
-	}
-	
-	/**
-	 * 
-	 * @param color
-	 * @return
-	 */
-	public boolean colorize(String color)
-	{
-        String url = KAROTZ_URL_LED + "?action=pulse&color=" + color
-        	+ "&period=3000&pulse=500&interactiveid=" + this.getInteractiveId();
+    /**
+     * @return String
+     */
+    public String getInteractiveId() {
+        if (null == KarotzClient.interactiveId) {
+            this.requestInteractiveId();
+        }
+        return KarotzClient.interactiveId;
+    }
+
+    /**
+     * @param textToSpeak
+     * @param language
+     * @return
+     */
+    public boolean speak(String textToSpeak, String language) {
+        textToSpeak = textToSpeak.replace(" ", "+");
+        /*
+         * try { textToSpeak = URLEncoder.encode(textToSpeak, "UTF-8"); } catch
+         * (UnsupportedEncodingException e) { logger.severe("Error encoding TTS:
+         * " + e.getMessage()); }
+         */
+
+        String url = KAROTZ_URL_TTS + "?action=speak&lang=" + language + "&text="
+                + textToSpeak + "&interactiveid=" + this.getInteractiveId();
 
         return this.doRequest(url);
-	}
-	
+    }
+
+    /**
+     *
+     * @param color
+     * @return
+     */
+    public boolean colorize(String color) {
+        String url = KAROTZ_URL_LED + "?action=pulse&color=" + color
+                + "&period=3000&pulse=500&interactiveid=" + this.getInteractiveId();
+
+        return this.doRequest(url);
+    }
+
     /**
      * @see http://dev.karotz.com/api/signed.html
      */
-    protected void requestInteractiveId()
-    {
+    protected void requestInteractiveId() {
         Random random = new Random();
         Map<String, String> parameters = new HashMap<String, String>();
         parameters.put("apikey", KAROTZ_API_KEY);
@@ -163,86 +157,84 @@ public class KarotzClient {
             logger.severe("Exception catched: " + e.toString());
             interactiveId = null;
         }
-        
+
         KarotzClient.interactiveId = this.parseXML(result);
     }
-    
-    protected String parseXML(String xml) {
-    	String value = null;
-    	
-    	try {
-			DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	        Document document = parser.parse(new InputSource(new StringReader(xml)));
-	        Element elt = (Element)document.getElementsByTagName("interactiveId").item(0);
-	       
-	        value = elt.getTextContent();
-	        
-	        logger.info("InteractiveId: " + value);
-		} catch (Exception e) {
-			logger.severe("Failed to parse: " + xml);
-		}
-		
-		return value;
-    }
-	
-	/**
-	 * @param String url	The URL to call.
-	 * @return boolean		<code>false</code> if the request has failed, <code>true</code> otherwise.
-	 */
-    protected boolean doRequest(String url)
-	{
-		URLConnection connection = null;
 
-		try {
-			connection = ProxyConfiguration.open(new URL(url));
-		} catch (Exception e) {
-			KarotzClient.interactiveId = null;
-			logger.severe(e.getMessage());
-			return false;
-		}
-		
-		try {
-			connection.connect();
-			connection.getInputStream();
-			logger.info("Connect to " + url);
-		} catch (IOException e) {
-			logger.severe(e.getMessage());
-			KarotzClient.interactiveId = null;
-			return false;
-		}
-		
-		return true;
-	}
-	
+    protected String parseXML(String xml) {
+        String value = null;
+
+        try {
+            DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = parser.parse(new InputSource(new StringReader(xml)));
+            Element elt = (Element) document.getElementsByTagName("interactiveId").item(0);
+
+            value = elt.getTextContent();
+
+            logger.info("InteractiveId: " + value);
+        } catch (Exception e) {
+            logger.severe("Failed to parse: " + xml);
+        }
+
+        return value;
+    }
+
+    /**
+     * @param String url	The URL to call.
+     * @return boolean
+     * <code>false</code> if the request has failed,
+     * <code>true</code> otherwise.
+     */
+    protected boolean doRequest(String url) {
+        URLConnection connection = null;
+
+        try {
+            connection = ProxyConfiguration.open(new URL(url));
+        } catch (Exception e) {
+            KarotzClient.interactiveId = null;
+            logger.severe(e.getMessage());
+            return false;
+        }
+
+        try {
+            connection.connect();
+            connection.getInputStream();
+            logger.info("Connect to " + url);
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+            KarotzClient.interactiveId = null;
+            return false;
+        }
+
+        return true;
+    }
+
     /**
      */
-	protected String hmacSha1(String data, String secretKey)
-    throws Exception {
+    protected String hmacSha1(String data, String secretKey)
+            throws Exception {
         Mac mac = Mac.getInstance("HmacSHA1");
         SecretKeySpec secret = new SecretKeySpec(secretKey.getBytes(), "HmacSHA1");
-        
+
         mac.init(secret);
         byte[] digest = mac.doFinal(data.getBytes());
-        
+
         return URLEncoder.encode(new String(Base64.encodeBase64String(digest)), "UTF-8").replace("%0D%0A", "");
     }
 
     /**
      */
     protected String getSignedUrl(Map<String, String> params, String secretKey)
-    throws Exception {
+            throws Exception {
         SortedMap<String, String> sortedParams = new TreeMap<String, String>(params);
         Iterator<Map.Entry<String, String>> iter = sortedParams.entrySet().iterator();
-        
+
         Map.Entry<String, String> next = iter.next();
         StringBuilder buffer = new StringBuilder(next.getKey()).append("=").append(next.getValue());
 
         while (iter.hasNext()) {
             next = iter.next();
-            buffer.append("&")
-                .append(next.getKey())
-                .append("=")
-                .append(next.getValue());
+            buffer.append("&").append(next.getKey()).append("=").append(next.getValue());
         }
 
         String signedQuery = this.hmacSha1(buffer.toString(), secretKey);
--- src/main/java/fr/willdurand/jenkins/karotz/KarotzPublisher.java
+++ src/main/java/fr/willdurand/jenkins/karotz/KarotzPublisher.java
@@ -11,16 +11,12 @@ import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.util.FormValidation;
-
 import java.io.IOException;
 import java.util.logging.Logger;
-
 import javax.servlet.ServletException;
-
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
-
 
 /**
  * KarotzPublisher class.
@@ -28,16 +24,18 @@ import org.kohsuke.stapler.QueryParameter;
  * @author William Durand <william.durand1@gmail.com>
  */
 public class KarotzPublisher extends Notifier {
+
     /**
      * Logger
      */
-	protected static Logger logger = Logger.getLogger(KarotzPublisher.class.getName());
-	/**
-	 * Install Id
-	 */
-	protected String installId;
+    protected static Logger logger = Logger.getLogger(KarotzPublisher.class.getName());
 
-	// Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    /**
+     * Install Id
+     */
+    protected String installId;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
     public KarotzPublisher(String installId) {
         this.installId = installId;
@@ -49,20 +47,21 @@ public class KarotzPublisher extends Notifier {
     public String getInstallId() {
         return this.installId;
     }
-    
+
     /**
-     * Descriptor for {@link KarotzPublisher}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
+     * Descriptor for {@link KarotzPublisher}. Used as a singleton. The class is
+     * marked as public so that it can be accessed from views.
      */
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+
         /**
          * Performs on-the-fly validation of the form field 'install id'.
          *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
+         * @param value This parameter receives the value that the user has
+         * typed.
+         * @return Indicates the outcome of the validation. This is sent to the
+         * browser.
          */
         public FormValidation doCheckInstallId(@QueryParameter String value)
                 throws IOException, ServletException {
@@ -77,7 +76,7 @@ public class KarotzPublisher extends Notifier {
         }
 
         @Override
-		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
             // Indicates that this publisher can be used with all kinds of project types
             return true;
         }
@@ -86,35 +85,35 @@ public class KarotzPublisher extends Notifier {
          * This human readable name is used in the configuration screen.
          */
         @Override
-		public String getDisplayName() {
+        public String getDisplayName() {
             return "Publish with your Karotz";
         }
     }
-    
+
     @Override
     public boolean perform(final AbstractBuild<?, ?> build, final Launcher launcher, final BuildListener listener)
-    throws InterruptedException, IOException {
+            throws InterruptedException, IOException {
         if (StringUtils.isBlank(this.getInstallId())) {
             listener.getLogger().println("No Karotz install id provided.");
             return false;
         }
 
         KarotzClient client = new KarotzClient(this.getInstallId());
-        String projectName  = build.getProject().getName();
-        
+        String projectName = build.getProject().getName();
+
         if (build.getResult() == Result.FAILURE) {
             this.onFailure(client, projectName);
         } else if (build.getResult() == Result.UNSTABLE) {
             this.onUnstable(client, projectName);
         } else if (build.getResult() == Result.SUCCESS) {
-        	// Build recover
-        	if (build.getPreviousBuild() != null && build.getPreviousBuild().getResult() == Result.FAILURE) {
-        		this.onRecover(client, projectName);
-        	} else {
-        		this.onSuccess(client, projectName);
-        	}
+            // Build recover
+            if (build.getPreviousBuild() != null && build.getPreviousBuild().getResult() == Result.FAILURE) {
+                this.onRecover(client, projectName);
+            } else {
+                this.onSuccess(client, projectName);
+            }
         }
-        
+
         return true;
     }
 
@@ -122,73 +121,68 @@ public class KarotzPublisher extends Notifier {
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.BUILD;
     }
-    
+
     /**
      * Prepare the text to speak (tts) by replacing variables with their values.
-     * 
+     *
      * @param String tts
      * @param String projectName
      * @return String
      */
-    protected String prepareTTS(String tts, String projectName)
-    {
-    	return StringUtils.replace(tts, "${projectName}", projectName);
+    protected String prepareTTS(String tts, String projectName) {
+        return StringUtils.replace(tts, "${projectName}", projectName);
     }
-    
+
     /**
-     * Triggered on  build failure.
-     * 
+     * Triggered on build failure.
+     *
      * @param KarotzClient client	A Karotz client
      * @param String projectName	A project name
      */
-    protected void onFailure(KarotzClient client, String projectName)
-    {
-    	String tts = prepareTTS("The project ${projectName} has failed", projectName);
-    	logger.info("TTS (failure):" + tts);
-    	
-    	client.speak(tts, "EN");
+    protected void onFailure(KarotzClient client, String projectName) {
+        String tts = prepareTTS("The project ${projectName} has failed", projectName);
+        logger.info("TTS (failure):" + tts);
+
+        client.speak(tts, "EN");
     }
-    
+
     /**
      * Triggered on build unstable.
-     * 
+     *
      * @param KarotzClient client	A Karotz client
      * @param String projectName	A project name
-
+     *
      */
-    protected void onUnstable(KarotzClient client, String projectName)
-    {
-    	String tts = prepareTTS("The project ${projectName} is unstable", projectName);
-    	logger.info("TTS (unstable):" + tts);
+    protected void onUnstable(KarotzClient client, String projectName) {
+        String tts = prepareTTS("The project ${projectName} is unstable", projectName);
+        logger.info("TTS (unstable):" + tts);
 
-    	client.speak(tts, "EN");    	
+        client.speak(tts, "EN");
     }
-    
+
     /**
      * Triggered on build recover.
-     * 
+     *
      * @param KarotzClient client	A Karotz client
      * @param String projectName	A project name
      */
-    protected void onRecover(KarotzClient client, String projectName)
-    {
-    	String tts = prepareTTS("The project ${projectName} is back to stable", projectName);
-    	logger.info("TTS (success):" + tts);
+    protected void onRecover(KarotzClient client, String projectName) {
+        String tts = prepareTTS("The project ${projectName} is back to stable", projectName);
+        logger.info("TTS (success):" + tts);
 
-    	client.speak(tts, "EN");
+        client.speak(tts, "EN");
     }
-    
+
     /**
      * Triggered on build success.
-     * 
+     *
      * @param KarotzClient client	A Karotz client
      * @param String projectName	A project name
      */
-    protected void onSuccess(KarotzClient client, String projectName)
-    {
-    	String tts = prepareTTS("The project ${projectName} is ok", projectName);
-    	logger.info("TTS (success):" + tts);
+    protected void onSuccess(KarotzClient client, String projectName) {
+        String tts = prepareTTS("The project ${projectName} is ok", projectName);
+        logger.info("TTS (success):" + tts);
 
-    	client.speak(tts, "EN");
+        client.speak(tts, "EN");
     }
 }
